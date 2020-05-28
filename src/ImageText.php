5a6cbf613730d4d302a5aa79f158c0f63653810e
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-05-28 00:28:05 +0000

    Fix default font on ImageText

--- src/ImageText.php
+++ src/ImageText.php
@@ -42,8 +42,9 @@ class ImageText
      */
     private $textBox;
 
-    public function __construct(string $text, string $hexColor="#000", string $fontPath=getcwd() ."/OpenSans.ttf", int $fontSize=20, float $fontScale=1, int $fontAngle=0)
+    public function __construct(string $text, string $hexColor="#000", string $fontPath=(__DIR__ . "/OpenSans.ttf"), int $fontSize=20, float $fontScale=1, int $fontAngle=0)
     {
+      // print_r($fontPath);die();
         $this->text = $text;
         $this->rgbColor = convertHexToRgb($hexColor);
         $this->fontPath = $fontPath;
